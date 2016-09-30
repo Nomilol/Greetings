9b933369933f7ee0946accc49f2ceb3d66527784
Axel <lezinaxel@gmail.com>
2016-09-30 10:34:31 +0200

    Ajout du code " récupération des infos utilisateurs "

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 console.log('have fun !');
+
+function hello(){
+	var lastName = $('#last_name').val();
+	var city = $('#city').val();
+
+
+	var infos = {
+		first_name : 'first name',
+		last_name : 'last name',
+		city : 'city'
+	}
+	$('button').click(function(){
+		var name = $('#first_name').val();
+		infos.first_name = name;
+		console.log(infos);
+		var lastName = $('#last_name').val();
+		infos.last_name = lastName;
+		console.log(infos);
+		var city = $('#city').val();
+		infos.city = city;
+		console.log(infos);
+
+
+		
+		var salut = $('#username').html(infos.last_name + " " + infos.first_name);
+
+		
+		
+
+	});
+
+};
+
+hello();
+/*Concaténez les champs first_name et last_name et 
+dites bonjour à notre cher visiteur dans le titre en 
+replacant le contenu du span #username*/
